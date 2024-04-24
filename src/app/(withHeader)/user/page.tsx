c46f031ccce3fa6f689542d7805eeddcd4fbfe82
marryp0ppins <nagordeev0106@yandex.ru>
2024-04-24 23:59:18 +0300

    search params to user page add

--- src/app/(withHeader)/user/page.tsx
+++ src/app/(withHeader)/user/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 import React, { memo, useCallback, useMemo, useState } from 'react';
+import { useRouter, useSearchParams } from 'next/navigation';
 
 import Text, { TEXT_VIEW } from '@/components/Text';
 import Profile from '@/containers/Profile';
@@ -11,15 +12,34 @@ import classes from './page.module.scss';
 const cnUserPage = classname(classes, 'userPage');
 
 enum Chapter {
-  Profile = 'Profile',
-  CREATED_SYSTEMS = ' createdSystems',
+  PROFILE = 'profile',
+  CREATED_SYSTEMS = 'systems',
   HISTORY = 'history',
 }
 
-const Page: React.FC = () => {
-  const [chapter, setChapter] = useState<Chapter>(Chapter.Profile);
+const getSection = (param: string | null): Chapter => {
+  switch (param) {
+    case 'systems':
+      return Chapter.CREATED_SYSTEMS;
+    case 'history':
+      return Chapter.HISTORY;
+    default:
+      return Chapter.PROFILE;
+  }
+};
 
-  const chapterChoice = useCallback((chptr: Chapter) => () => setChapter(chptr), []);
+const Page: React.FC = () => {
+  const searchParams = useSearchParams();
+  const router = useRouter();
+  const [chapter, setChapter] = useState<Chapter>(getSection(searchParams.get('section')));
+
+  const chapterChoice = useCallback(
+    (chptr: Chapter) => () => {
+      router.replace(`/user?section=${chptr}`);
+      setChapter(chptr);
+    },
+    [router],
+  );
 
   const memoChapter = useMemo(() => {
     switch (chapter) {
@@ -36,9 +56,9 @@ const Page: React.FC = () => {
     <div className={cnUserPage()}>
       <header className={cnUserPage('header')}>
         <Text
-          onClick={chapterChoice(Chapter.Profile)}
+          onClick={chapterChoice(Chapter.PROFILE)}
           view={TEXT_VIEW.p18}
-          className={cnUserPage('chapter', { selected: chapter === Chapter.Profile })}
+          className={cnUserPage('chapter', { selected: chapter === Chapter.PROFILE })}
         >
           Профиль
         </Text>
--- src/providers/privateRouteProvider.tsx
+++ src/providers/privateRouteProvider.tsx
@@ -1,18 +1,19 @@
 'use client';
 import { ReactNode, useLayoutEffect } from 'react';
 import Cookies from 'js-cookie';
-import { redirect, usePathname } from 'next/navigation';
+import { redirect, usePathname, useSearchParams } from 'next/navigation';
 
 const allowURL = ['/', '/login', '/registration'];
 
 export const PrivateRouterProvider = ({ children }: { children: ReactNode }) => {
   const pathname = usePathname();
+  const searchParams = useSearchParams();
 
   useLayoutEffect(() => {
     if (!Cookies.get('session_id') && !allowURL.includes(pathname)) {
-      redirect(`/login?back_uri=${pathname}`);
+      redirect(`/login?back_uri=${pathname}?${searchParams.toString()}`);
     }
-  }, [pathname]);
+  }, [pathname, searchParams]);
 
   return children;
 };
