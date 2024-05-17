dc0b18734ad32600245363e462f8f8adb9cdfe9c
marryp0ppins <nagordeev0106@yandex.ru>
2024-05-17 06:55:01 +0300

    instructions add

--- src/app/(withHeader)/instruction/page.tsx
+++ src/app/(withHeader)/instruction/page.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import { INSTRACTIONS } from '@/constants';
 import { classname } from '@/utils';
 
 import classes from './page.module.scss';
@@ -7,7 +8,17 @@ import classes from './page.module.scss';
 const cnInstructionPage = classname(classes, 'instruсtion');
 
 const Page: React.FC = () => {
-  return <div className={cnInstructionPage()}>инструкция</div>;
+  return (
+    <div className={cnInstructionPage()}>
+      {INSTRACTIONS.map((text, index) => (
+        <div key={index}>
+          {text.text.map((line, lineIndex) => (
+            <p key={lineIndex}>{line}</p>
+          ))}
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Page;
