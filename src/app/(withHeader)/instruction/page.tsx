e21240613e8d3bb59305508ea4387ebd2aa88fff
marryp0ppins <nagordeev0106@yandex.ru>
2024-06-11 15:49:39 +0300

    instration fix

--- src/app/(withHeader)/instruction/page.tsx
+++ src/app/(withHeader)/instruction/page.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import Text, { TEXT_TAG, TEXT_VIEW } from '@/components/Text';
 import { INSTRACTIONS } from '@/constants';
 import { classname } from '@/utils';
 
@@ -10,6 +11,9 @@ const cnInstructionPage = classname(classes, 'instruсtion');
 const Page: React.FC = () => {
   return (
     <div className={cnInstructionPage()}>
+      <Text view={TEXT_VIEW.title} tag={TEXT_TAG.div} className={cnInstructionPage('title')}>
+        Инструкция по созданию экспертных систем
+      </Text>
       {INSTRACTIONS.map((text, index) => (
         <div key={index}>
           {text.text.map((line, lineIndex) => (
